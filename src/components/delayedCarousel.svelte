074b846062b13125d50f87fbdb4abf4d9e48a73c
Peter Krauß <peter.krauss@kit.edu>
2021-12-14 16:19:13 +0100

    initial commit

---
+++ src/components/delayedCarousel.svelte
@@ -0,0 +1,17 @@
+<script>
+    import { onMount } from 'svelte'
+
+    let Carousel // for saving Carousel component class
+
+    onMount(async () => {
+        console.log('Seem this gets never executed ... ')
+        const module = await import('svelte-carousel')
+        Carousel = module.default
+    })
+</script>
+
+<svelte:component this={Carousel}>
+    <div>1</div>
+    <div>2</div>
+    <div>3</div>
+</svelte:component>
